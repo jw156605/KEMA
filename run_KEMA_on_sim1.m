a347077e65b95ae72f673706830404b68683761e
jwelch <jwelch@login03.broadinstitute.org>
2017-08-23 11:52:31 -0400

    Updated example code

--- run_KEMA_on_sim1.m
+++ run_KEMA_on_sim1.m
@@ -1,4 +1,3 @@
-addpath('general_routine')
 load('simulated_data1')
 domain1_labeled = struct('X',X1,'Y',Y1);
 domain2_labeled = struct('X',X2,'Y',Y2);
@@ -15,10 +14,8 @@ domain1_test = struct('X',XT1,'Y',YT1);
 domain2_test = struct('X',XT2,'Y',YT2);
 test = {domain1_test,domain2_test};
 [proj] = KMAproject(labeled,unlabeled,test,evects,opts);
-P1 = proj{1,1}.train;
-P2 = proj{1,2}.train;
-%P1 = proj{1,1}.test;
-%P2 = proj{1,2}.test;
+P1 = proj{1,1}.test;
+P2 = proj{1,2}.test;
 
 close all;
 scatter(P1(1,:)',P1(2,:)');
