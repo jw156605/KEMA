1dc9d772fdf225e81554f95ff2f4fbad70be369b
Joshua Welch <jwelch@cs.unc.edu>
2017-08-25 09:10:40 -0400

    Add params to SNN
    Add params to SNN

--- general_routine/build_SNN_graph.R
+++ general_routine/build_SNN_graph.R
@@ -62,10 +62,10 @@ CalcSNNSparse <- function(
 args = commandArgs(trailingOnly=TRUE)
 file_in   = args[1]
 file_out  = args[2]
-num_ICs   = args[3]
-k.param   = args[4]
-k.scale   = args[5]
-prune.SNN = args[6]
+num_ICs   = as.numeric(args[3])
+k.param   = as.numeric(args[4])
+k.scale   = as.numeric(args[5])
+prune.SNN = as.numeric(args[6])
 
 print("Reading data...")
 cell.data = read.csv(file_in,header=FALSE)
